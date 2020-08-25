e60bd4cad4ec7a5842f3dd0841a4168562ec5442
Micah Neitz <micalobia2@gmail.com>
2020-08-25 06:57:08 -0700

    Made IntRange a little less extra

--- Data/IntRange.cs
+++ Data/IntRange.cs
@@ -8,47 +8,24 @@ using System.Threading.Tasks;
 
 namespace Cript.Data
 {
-    [StructLayout(LayoutKind.Explicit,Size=Size)]
     struct IntRange
     {
         public const int Size = 10;
-
-        [FieldOffset(0)] private byte[] _bytes;
-        [FieldOffset(0)] private int _left;
-        [FieldOffset(4)] private int _right;
-        [FieldOffset(8)] private bool _leftOpen;
-        [FieldOffset(9)] private bool _rightOpen;
+        
+        private int _left;
+        private int _right;
+        private bool _leftOpen;
+        private bool _rightOpen;
 
         public int Left { get => _left; set => _left = value; }
         public int Right { get => _right; set => _right = value; }
         public bool LeftOpen { get => _leftOpen; set => _leftOpen = value; }
         public bool RightOpen { get => _rightOpen; set => _rightOpen = value; }
         public bool Open => LeftOpen && RightOpen;
-        public byte[] Bytes
-        {
-            get
-            {
-                if (_bytes == null)
-                {
-                    using (MemoryStream m = new MemoryStream(new byte[Size]))
-                    using (BinaryWriter b = new BinaryWriter(m))
-                    {
-                        b.Write(Left);
-                        b.Write(Right);
-                        b.Write(LeftOpen);
-                        b.Write(RightOpen);
-                        m.Seek(0, SeekOrigin.Begin);
-                        _bytes = m.ToArray();
-                    }
-                }
-                return _bytes;
-            }
-        }
 
         public IntRange(int left, int right) : this(left, right, false, false) { }
         public IntRange(int left, int right, bool leftOpen, bool rightOpen)
         {
-            _bytes = new byte[Size];
             _left = left;
             _right = right;
             _leftOpen = leftOpen;
