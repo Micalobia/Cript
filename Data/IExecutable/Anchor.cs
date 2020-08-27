199d40ecc00b120d4af77d911b7c00d5d0ae20fc
Micah <micalobia2@gmail.com>
2020-08-27 06:12:23 -0700

    Changed Anchor
    Made it similar to Align, to make things uniform

--- Data/IExecutable/Anchor.cs
+++ Data/IExecutable/Anchor.cs
@@ -2,24 +2,15 @@
 {
     public class Anchor : IExecutable
     {
-        public const int None = 0;
-        public const int Feet = 1;
-        public const int Eyes = 2;
-        public int Value { get; private set; }
-        public Anchor() => Value = None;
-        public Anchor(int value) => Value = value & 0b11;
-        private bool HasFlag(int type) => (Value & type) > 0;
-        public override string ToString()
-        {
-            switch(Value)
-            {
-                case Feet: return "feet";
-                case Eyes: return "eyes";
-                case None:
-                default:
-                    return "none";
-            }
-        }
-        public string GetExecute(int type) => Value == None ? null : $"anchored {ToString()}";
+        public readonly Anchor None = new Anchor(_none);
+        public readonly Anchor Feet = new Anchor(_feet);
+        public readonly Anchor Eyes = new Anchor(_eyes);
+        private const string _none = null;
+        private const string _feet = "feet";
+        private const string _eyes = "eyes";
+        private string _value;
+        private Anchor(string value) => _value = value;
+        public override string ToString() => _value;
+        public string GetExecute(int type) => _value == null ? null : $"anchored {_value}";
     }
 }
