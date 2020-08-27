92a9a5c6296a1c53919c66a46fedbff8561755ff
Micah Neitz <micalobia2@gmail.com>
2020-08-27 02:12:28 -0700

    Added ScanMode enumerator

---
+++ Data/Enums/ScanMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript.Data.Enums
+{
+    public enum ScanMode
+    {
+        All = 0,
+        Masked = 1
+    }
+}
