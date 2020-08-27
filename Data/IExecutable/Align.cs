434423797f24d9ee8bd6ed4832ce5abab4922215
Micah Neitz <micalobia2@gmail.com>
2020-08-26 17:00:33 -0700

    Changed the Align class, it's basically an enum with methods now

--- Data/IExecutable/Align.cs
+++ Data/IExecutable/Align.cs
@@ -7,27 +7,31 @@ namespace Cript.Data
 {
     public sealed class Align : IExecutable
     {
-        public const int None = 0;
-        public const int X = 1;
-        public const int Y = 2;
-        public const int XY = 3;
-        public const int YX = 3;
-        public const int Z = 4;
-        public const int ZX = 5;
-        public const int XZ = 5;
-        public const int ZY = 6;
-        public const int YZ = 6;
-        public const int XYZ = 7;
-        public const int XZY = 7;
-        public const int YXZ = 7;
-        public const int YZX = 7;
-        public const int ZXY = 7;
-        public const int ZYX = 7;
-        public int Value { get; private set; }
-        public Align() => Value = None;
-        public Align(int type) => Value = type & XYZ;
-        private bool HasFlag(int type) => (Value & type) > 0;
-        public override string ToString() => string.Format("{0}{1}{2}", HasFlag(X) ? "x" : "", HasFlag(Y) ? "y" : "", HasFlag(Z) ? "z" : "");
-        public string GetExecute(int type) => Value == None ? null : $"align {ToString()}";
+        public static readonly Align None = new Align(_none);
+        public static readonly Align X = new Align(_x);
+        public static readonly Align Y = new Align(_y);
+        public static readonly Align Z = new Align(_z);
+        public static readonly Align XY = new Align(_x | _y);
+        public static readonly Align XZ = new Align(_x | _z);
+        public static readonly Align YX = XY;
+        public static readonly Align YZ = new Align(_y | _z);
+        public static readonly Align ZX = XZ;
+        public static readonly Align ZY = YZ;
+        public static readonly Align XYZ = new Align(_xyz);
+        public static readonly Align XZY = XYZ;
+        public static readonly Align YXZ = XYZ;
+        public static readonly Align YZX = XYZ;
+        public static readonly Align ZXY = XYZ;
+        public static readonly Align ZYX = XYZ;
+        private const int _none = 0;
+        private const int _x = 1;
+        private const int _y = 2;
+        private const int _z = 4;
+        private const int _xyz = _x | _y | _z;
+        private readonly int _value;
+        private Align(int type) => _value = type & _xyz;
+        private bool HasFlag(int type) => (_value & type) > 0;
+        public override string ToString() => string.Format("{0}{1}{2}", HasFlag(_x) ? "x" : "", HasFlag(_y) ? "y" : "", HasFlag(_z) ? "z" : "");
+        public string GetExecute(int type) => _value == _none ? null : $"align {ToString()}";
     }
 }
