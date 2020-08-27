40d24b3766b311a3ee983d905f30f6c439b825f6
Micah <micalobia2@gmail.com>
2020-08-27 02:56:50 -0700

    Added Scoreboard struct

---
+++ Data/Scoreboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript.Data
+{
+    struct Scoreboard
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value == null ? throw new ArgumentNullException("Name cannot be null") : value.Length > 0 && value.Length <= 16 ? _name = value : throw new ArgumentException("Has to be 1-16 characters");
+        }
+        public Scoreboard(string name) : this() => Name = name;
+    }
+}
