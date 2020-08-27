86dc3f6fa4ed9bbd222152b035eb596a43bcd3f7
Micah Neitz <micalobia2@gmail.com>
2020-08-27 02:22:49 -0700

    Added Comparison enumerator

---
+++ Data/Enums/Comparison.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript.Data
+{
+    public enum Comparison
+    {
+        LessThan = 0,
+        LessThanOrEqualTo = 1,
+        EqualTo = 2,
+        GreaterThanOrEqualTo = 3,
+        GreaterThan = 4
+    }
+}
