d986775d49663683aeadfeba0d1b1ee9acda17b3
Micah <micalobia2@gmail.com>
2020-08-27 03:01:29 -0700

    Changed Scoreboard struct a bit

--- Data/Scoreboard.cs
+++ Data/Scoreboard.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cript.Data
 {
-    struct Scoreboard
+    public struct Scoreboard
     {
         private string _name;
         public string Name
@@ -15,5 +15,18 @@ namespace Cript.Data
             set => _name = value == null ? throw new ArgumentNullException("Name cannot be null") : value.Length > 0 && value.Length <= 16 ? _name = value : throw new ArgumentException("Has to be 1-16 characters");
         }
         public Scoreboard(string name) : this() => Name = name;
+
+        public override string ToString() => Name;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Scoreboard))
+                return false;
+
+            var scoreboard = (Scoreboard)obj;
+            return Name == scoreboard.Name;
+        }
+
+        public override int GetHashCode() => 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
     }
 }
