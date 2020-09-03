ea23458cb56c91ff7c10dc40ca1c2ca6ac284c04
Micah <micalobia2@gmail.com>
2020-09-03 06:13:35 -0700

    Added Datapack file
    Need to add a list of namespaces, that's gonna be a separate class and it'll take some work

---
+++ Datapack/Datapack.cs
@@ -0,0 +1,31 @@
+﻿using Cript.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript.Datapack
+{
+    public class Datapack
+    {
+        private string _iconPath;
+        private MCMeta _mcmeta;
+        private MinecraftVersion _version;
+        //TODO: Add list of namespaces
+
+        public string IconPath { get => _iconPath; set => _iconPath = value; }
+        public string Description { get => _mcmeta.Description; set => _mcmeta.Description = value; }
+        public MinecraftVersion Version
+        {
+            get => _version;
+            set
+            {
+                _version = value;
+                _mcmeta.PackFormat = value.PackFormat();
+            }
+        }
+    }
+}
