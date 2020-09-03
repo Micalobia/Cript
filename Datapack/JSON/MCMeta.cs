ff6a9ff267f0c36e4c2222a80a2d00f6703f1817
Micah <micalobia2@gmail.com>
2020-09-03 06:12:29 -0700

    Added MCMeta JSONObject

---
+++ Datapack/JSON/MCMeta.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript.Datapack
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class MCMeta
+    {
+        [JsonProperty] private Pack pack { get; set; }
+        public string Description { get => pack.Description; set => pack.Description = value; }
+        public int PackFormat { get => pack.PackFormat; set => pack.PackFormat = value; }
+        [JsonObject(MemberSerialization.OptIn)]
+        private class Pack
+        {
+            [JsonProperty(PropertyName = "pack_format")] public int PackFormat { get; set; }
+            [JsonProperty(PropertyName = "description")] public string Description { get; set; }
+        }
+        public MCMeta() => pack = new Pack();
+    }
+}
