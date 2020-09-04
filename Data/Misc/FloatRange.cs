5eec53c25cb8a6c8f33f6f7a5396a927db6e4989
Micah <micalobia2@gmail.com>
2020-09-04 01:43:30 -0700

    oop

--- Data/Misc/FloatRange.cs
+++ Data/Misc/FloatRange.cs
@@ -10,21 +10,21 @@ namespace Cript.Data.Misc
     [JsonObject(MemberSerialization.OptIn)]
     public struct FloatRange
     {
-        private int _left;
-        private int _right;
+        private float _left;
+        private float _right;
         private bool _leftOpen;
         private bool _rightOpen;
 
-        [JsonProperty("min")] public int Left { get => _left; set => _left = value; }
-        [JsonProperty("max")] public int Right { get => _right; set => _right = value; }
+        [JsonProperty("min")] public float Left { get => _left; set => _left = value; }
+        [JsonProperty("max")] public float Right { get => _right; set => _right = value; }
         public bool LeftOpen { get => _leftOpen; set => _leftOpen = value; }
         public bool RightOpen { get => _rightOpen; set => _rightOpen = value; }
         public bool Open => LeftOpen && RightOpen;
         public bool HasOpen => LeftOpen || RightOpen;
         public bool Skewed => LeftOpen ^ RightOpen;
 
-        public FloatRange(int left, int right) : this(left, right, false, false) { }
-        public FloatRange(int left, int right, bool leftOpen, bool rightOpen)
+        public FloatRange(float left, float right) : this(left, right, false, false) { }
+        public FloatRange(float left, float right, bool leftOpen, bool rightOpen)
         {
             _left = left;
             _right = right;
@@ -32,7 +32,7 @@ namespace Cript.Data.Misc
             _rightOpen = rightOpen;
         }
 
-        public bool InRange(int value) => (LeftOpen || value >= Left) && (RightOpen || value <= Right);
+        public bool InRange(float value) => (LeftOpen || value >= Left) && (RightOpen || value <= Right);
 
         public bool ShouldSerializeLeft() => !LeftOpen;
         public bool ShouldSerializeRight() => !RightOpen;
